0f0811c0d1cd8441ba197594c0ce48d7cfe8424c
Duane Arzu <arzujanathan7@gmail.com>
2024-09-09 12:57:42 -0600

    second commit

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -9,7 +9,7 @@ func (a *applicationDependencies)healthcheckHandler(w http.ResponseWriter,
                                                r *http.Request) {
     jsResponse := `{"status": "available", "environment": %q, "version": %q}`
     jsResponse = fmt.Sprintf(jsResponse, a.config.environment, appVersion)
-    // w.Header().Set("Content-Type", "application/json")
+    w.Header().Set("Content-Type", "application/json")
     w.Write([]byte(jsResponse))
 
     // fmt.Fprintln(w, "status: available")
