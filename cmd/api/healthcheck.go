86f291cee5bb2634c360993137a89afb71303fcb
Duane Arzu <arzujanathan7@gmail.com>
2024-09-30 13:08:06 -0600

    fourth commit jsv4

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,32 +1,23 @@
 package main
 
 import (
-  "encoding/json"
   "net/http"
 )
 
 func (a *applicationDependencies)healthcheckHandler(w http.ResponseWriter,
                                                r *http.Request) {
-   
- data := map[string]string {
-  "status": "available",
-  "environment": a.config.environment,
-  "version": appVersion,
- }
- 
- jsResponse, err := json.Marshal(data)
- if err != nil {
-     a.logger.Error(err.Error())
-     http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-     return
- }
- 
- jsResponse = append(jsResponse, '\n')
- w.Header().Set("Content-Type", "application/json")
- w.Write(jsResponse)
-
- return
-}
+data := map[string]string {
+          "status": "available",
+          "environment": a.config.environment,
+          "version": appVersion,
+                        }
+err := a.writeJSON(w, http.StatusOK, data, nil)
+if err != nil {
+ a.logger.Error(err.Error())
+ http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+  }
+   }
+                                             
                                                 // jsResponse := `{"status": "available", "environment": %q, "version": %q}`
     // jsResponse = fmt.Sprintf(jsResponse, a.config.environment, appVersion)
     // w.Header().Set("Content-Type", "application/json")
