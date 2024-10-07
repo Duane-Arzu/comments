ab3c626366ba9ba18ca610c9c2f9956de042666e
Duane Arzu <arzujanathan7@gmail.com>
2024-10-07 13:44:08 -0600

    recover panic

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,7 +15,9 @@ func (a *applicationDependencies)routes() http.Handler  {
 	router.MethodNotAllowed = http.HandlerFunc(a.methodNotAllowedResponse)
 	// setup routes
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", a.healthcheckHandler)
-	return router
+	router.HandlerFunc(http.MethodPost, "/v1/comments", a.createCommentHandler)
+	
+	return a.recoverPanic(router)
    
  }
  
