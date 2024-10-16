0b99aef38faee951ef0573289a24dfcd0385bebf
Duane-Arzu <73129799+Duane-Arzu@users.noreply.github.com>
2024-10-16 13:10:05 -0600

    Add files via upload

---
+++ cmd/api/comments.go
@@ -0,0 +1,34 @@
+// Filename: cmd/api/comments.go
+package main
+
+import (
+  
+  "fmt"
+  "net/http"
+  // import the data package which contains the definition for Comment
+  _ "github.com/Duane-Arzu/comments/internal/data"
+)
+
+func (a *applicationDependencies)createCommentHandler(w http.ResponseWriter,
+	r *http.Request) { 
+// create a struct to hold a comment
+// we use struct tags[``] to make the names display in lowercase
+var incomingData struct {
+Content  string  `json:"content"`
+Author   string  `json:"author"`
+}  
+
+
+
+
+//perform the decoding
+
+err := a.readJSON(w, r, &incomingData)
+if err != nil {
+	a.badRequestResponse(w, r, err)
+	return
+}
+
+//for now display the result
+fmt.Fprintf(w, "%+v\n", incomingData)
+}
